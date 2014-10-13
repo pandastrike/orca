cd2fc44735e013742708dd7db44b11296e97e750
David Harper <david@pandastrike.com>
2014-10-13 14:24:30 -0700

    Added Elasticsearch and Kibana Source

---
+++ src/elasticsearch-1.4.0.Beta1/plugins/marvel/_site/sense/app/kb/api_0_90/count.js
@@ -0,0 +1,1 @@
+define([],function(){return function(e){e.addEndpointDescription("_search",{methods:["GET","POST"],priority:10,patterns:["{indices}/{types}/_count","{indices}/_count","_count"],url_params:{preference:["_primary","_primary_first","_local","_only_node:xyz","_prefer_node:xyz","_shards:2,3"],routing:"",min_score:1},data_autocomplete_rules:{__scope_link:"GLOBAL.query"}})}});
---
+++ src/elasticsearch-1.4.0.Beta1/plugins/marvel/_site/sense/app/kb/api_0_90/nodes.js
@@ -0,0 +1,1 @@
+define([],function(){return function(n){n.addEndpointDescription("_nodes/hot_threads",{methods:["GET"],patterns:["_nodes/hot_threads","_nodes/{nodes}/hot_threads"]})}});
