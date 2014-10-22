dde0018f540d0306f4c35592e573569dede6a77a
David Harper <david@pandastrike.com>
2014-10-22 01:41:15 -0700

    Please Be Sweet Scripting Victory

--- scripts/kibana-config.bash
+++ scripts/kibana-config.bash
@@ -2,34 +2,19 @@
 # This script writes a config file for Kibana that points it to the IP address of
 # the machine that is running Elasticsearch.
 
+# Lookup the IP address of Elasticsearch, so we know where to point Kibana.
+# TODO: Make this etcd endpoint dynamically assignable.  You can lookup `docker0` under
+#       `ip address show` while on a CoreOS machine, but I'm having some trouble assigning it.
+curl -L http://10.1.42.1:4001/v2/keys/dns/elasticsearch/host > temp.json
+host=$(cat temp.json | jq -r '.node.value')
+
+# Modify kibana.yml by adding the IP address of the Elasticsearch cluster.
 cd /data/kibana/kibana-4.0.0-BETA1.1/config
+sed s/localhost/$host/ kibana.yml > new.yml
+mv new.yml kibana.yml
 
-echo '# Kibana is served by a backend server. This controls which port to use.' > kibana.yml
-echo 'port: 5601' >> kibana.yml
-echo ' ' >> kibana.yml
 
-echo '# The Elasticsearch instance to use for all your queries' >> kibana.yml
-echo "elasticsearch: \"http://$1\"" >> kibana.yml
-echo ' ' >> kibana.yml
-
-echo '# Kibana uses an index in Elasticsearch to store saved searches, visualizations' >> kibana.yml
-echo "# and dashboards. It will create an new index if it doesn't already exist." >> kibana.yml
-echo 'kibanaIndex: "kibana-int"'  >> kibana.yml
-echo ' ' >> kibana.yml
-
-echo '# Applications loaded and included into Kibana. Use the settings below to' >> kibana.yml
-echo '# customize the applications and thier names.' >> kibana.yml
-echo 'apps:' >> kibana.yml
-
-echo '  - { id: "discover", name: "Discover" }' >> kibana.yml
-echo '  - { id: "visualize", name: "Visualize" }' >> kibana.yml
-echo '  - { id: "dashboard", name: "Dashboard" }' >> kibana.yml
-echo '  - { id: "settings", name: "Settings" }' >> kibana.yml
-
-echo '# The default application to laad.' >> kibana.yml
-echo 'defaultAppId: "discover"' >> kibana.yml
-
-echo "Configuration Complete.  Kibana is pointed at \"http://$1\""
+echo "Configuration Complete.  Kibana is pointed at \"http://$host:9200\""
 echo " "
 echo " "
 echo "Staring Kibana 4 Service..."
